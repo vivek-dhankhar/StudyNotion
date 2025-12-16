c361d6ec24a30dacfabf4f7e00e2dfc98a0c1123
vivekdhankhar <singhvivek23112000@gmail.com>
2025-12-16 14:00:35 +0530

    verify signature

--- controller/Payments.js
+++ controller/Payments.js
@@ -1,3 +1,4 @@
+const { default: webhooks } = require("razorpay/dist/types/webhooks")
 const {instance} = require("../config/razorpay")
 const Course = require('../models/Course')
 const User = require('../models/User')
@@ -53,4 +54,74 @@ exports.capturePayment=async(req,res)=>{
         console.log(error)
         return res.status(500).json({success:false,message:error.message})
     }
+}
+
+//verify signature of razorpay and server
+exports.verifySignature=async(req,res)=>{
+    //this is the weebhook/key is known by server(same entered in razorpay account)
+    const webhookSecret = "123456"
+
+    //signature will be send by the rezorpay in header(when payment is successfull, razorpay will hit an api given by account holder)
+    const signature = req.headers("x-razorpay-signature")
+
+    //convert webhook into signature using hash(Hmac)
+    const shasum = crypto.createHmac("sha256",webhookSecret)
+    shasum.update(JSON.stringify(req.body))
+    const digest = shasum.digest("hex")
+
+    if(signature === digest){
+        console.log("payment is authorized")
+
+        const {courseId , userId} = req.body.payload.payment.entity.notes;
+
+        try {
+            //fullfill the action
+
+            //find the course and enroll the student 
+            const enrolledCourse = await Course.findByIdAndUpdate(courseId , 
+                                                        {$push:{
+                                                            studentEnrolled:userId
+                                                        }},
+                                                        {new:true }
+            )
+            
+            if(!enrolledCourse){
+                return res.status(500).json({success:false,
+                                              message:"Course not found "
+                })
+            }
+
+            console.log(enrolledCourse)
+
+            //find the student and add the course to their enrolled course
+            const enrolledStudent = await User.findByIdAndUpdate(userId ,
+                                                     {$push:{
+                                                        courses:courseId
+                                                     }},
+                                                     {new:true}
+            )
+
+            console.log(enrolledStudent )
+
+            //send confirmation mail
+            const emailResponse = await mailSender(enrolledStudent.email , 
+                                                   "Sucessfully enrolled in course" ,
+                                                   "Congrulations , you successfully enrolld in the course !!"
+            )
+
+            console.log(emailResponse)
+            return res.status(200).json({success:true, 
+                                         message:"signature verified and course added"})
+             
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({success:false, message:error.message})
+        }
+    }
+    else{
+        return res.status(400).json({success:false ,
+                                     message:"invalid request"
+        }) 
+    }
+
 }
