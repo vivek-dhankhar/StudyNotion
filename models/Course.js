f54035a5a9fdbe2618406de37e48e57f19055847
vivekdhankhar <singhvivek23112000@gmail.com>
2025-12-17 12:25:01 +0530

    update

--- models/Course.js
+++ models/Course.js
@@ -1,48 +1,63 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-   courseName:{
-    type:String
-   },
-   courseDescription:{
-    type:String,
-   },
-   instructor:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true,
-    ref:"User"
-   },
-   whatYouWillLearn:{
-    type:String,
-   },
-   courseContent:[
-    {
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Section"
-   }
-   ],
-   ratingAndReviews:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"RatingAndReviews"
-   },
-   price:{
-    type:String,
-   },
-   thumbnail:{
-    type:String,
-   },
-   tag:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Tag"
-   },
-   studentEnrolled:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        require:true,
-    }
-   ]
-})
+// Define the Courses schema
+const coursesSchema = new mongoose.Schema({
+	courseName: { type: String },
+	courseDescription: { type: String },
+	instructor: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "user",
+	},
+	whatYouWillLearn: {
+		type: String,
+	},
+	courseContent: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Section",
+		},
+	],
+	ratingAndReviews: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "RatingAndReview",
+		},
+	],
+	price: {
+		type: Number,
+	},
+	thumbnail: {
+		type: String,
+	},
+	tag: {
+		type: [String],
+		required: true,
+	},
+	category: {
+		type: mongoose.Schema.Types.ObjectId,
+		// required: true,
+		ref: "Category",
+	},
+	studentsEnrolled: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "user",
+		},
+	],
+	instructions: {
+		type: [String],
+	},
+	status: {
+		type: String,
+		enum: ["Draft", "Published"],
+	},
+	createdAt: {
+		type:Date,
+		default:Date.now
+	},
+});
 
-const Course = mongoose.model('Course' , courseSchema)
-module.exports = Course
+// Export the Courses model
+module.exports = mongoose.model("Course", coursesSchema);
--- models/RatingAndReview.js
+++ models/RatingAndReview.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ratingAndReviewSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
+        required:true,
         ref:"User"
     },
     rating:{
@@ -12,7 +13,13 @@ const ratingAndReviewSchema = new mongoose.Schema({
     review:{
         type:String,
         required:true,
-    }
+    },
+    course: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "Course",
+		index: true,
+	},
 })
 
 module.exports = mongoose.model("RatingAndReview",ratingAndReviewSchema)
